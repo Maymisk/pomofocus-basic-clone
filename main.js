e099fbc75c1fce553a61715d0d6826a20e476848
Khalil M. M. Bohner <khalilbohner@gmail.com>
2021-12-14 19:31:13 -0300

    Bug fixes + lotsa stuff

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
-// let time = 0
+const timer = {
+    getTime() {
+        let formattedTime
+        App.time -= 1000
 
-// function getTime() {
-//     time += 1000
-//     let seconds = (time % (60 * 1000)) / 1000
-//     let minutes = time / (60 * 1000)
+        // Math
 
-//     minutes = String(minutes).split('.')[0]
+        let seconds = (App.time % (60 * 1000)) / 1000
+        let minutes = Math.floor(App.time / (60 * 1000))
 
-//     switch (true) {
-//         case minutes < 1 && seconds < 10:
-//             console.log(`00:0${seconds}`)
-//             break
+        // Formatting each part
 
-//         case minutes >= 1 && minutes < 10 && seconds >= 10:
-//             console.log(`0${minutes}:${seconds}`)
-//             break
+        let displayMinutes = minutes < 10 ? '0' + minutes : minutes
+        let displaySeconds = seconds < 10 ? '0' + seconds : seconds
 
-//         case minutes >= 1 && minutes < 10 && seconds < 10:
-//             console.log(`0${minutes}:0${seconds}`)
-//             break
+        formattedTime = `${displayMinutes}:${displaySeconds}`
 
-//         case minutes >= 10 && seconds < 10:
-//             console.log(`${minutes}:0${seconds}`)
-//             break
+        App.update(formattedTime)
+    },
 
-//         default:
-//             console.log(`${minutes}:${seconds}`)
-//     }
-// }
-// setInterval(getTime, 1000)
+    startTimer() {
+        this.timerInterval = setInterval(this.getTime, 1000)
+        DOM.timerButton.textContent = 'STOP'
+
+        DOM.removeStartEvent(DOM.timerButton)
+        DOM.addStopEvent(DOM.timerButton)
+
+        DOM.skipTimerButton.classList.remove('hidden')
+    },
+
+    stopTimer() {
+        clearInterval(this.timerInterval)
+        DOM.timerButton.textContent = 'START'
+
+        DOM.removeStopEvent(DOM.timerButton)
+        DOM.addStartEvent(DOM.timerButton)
+
+        DOM.skipTimerButton.classList.add('hidden')
+    }
+}
+
+const DOM = {
+    timerDiv: document.querySelector('#timer'),
+    timerButton: document.querySelector('#timerButton'),
+    skipTimerButton: document.querySelector('.skipTimerButton'),
+
+    addStartEvent(element) {
+        // this.start is necessary due to event listeners' properties
+
+        element.addEventListener(
+            'click',
+            (this.start = () => timer.startTimer())
+        )
+    },
+
+    addStopEvent(element) {
+        element.addEventListener('click', (this.stop = () => timer.stopTimer()))
+    },
+
+    removeStartEvent(element) {
+        element.removeEventListener('click', this.start)
+    },
+
+    removeStopEvent(element) {
+        element.removeEventListener('click', this.stop)
+    }
+}
+
+const App = {
+    init() {
+        this.time = 1000 * 60 * 25
+        DOM.addStartEvent(DOM.timerButton)
+    },
+
+    update(time) {
+        DOM.timerDiv.textContent = time
+    }
+}
+
+App.init()
